6befa919ad5248d1c6cc77262329fb0a50bbea5e
akilika <angelikaakinina05@gmail.com>
2025-02-12 10:42:00 +0300

    Homework Done

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,35 @@
 public class Main {
 
-    public static String checkingLeapYear(int year) {
+   public static void checkingLeapYear(int year) {
         boolean leapYear = ((year % 400) == 0) || (((year % 4) == 0) && ((year % 100) != 0));
         if (leapYear) {
-            return year + " год - високосный";
+            System.out.println(year + " год - високосный");
         } else {
-            return year + " год - невисокосный";
+            System.out.println(year + " год - невисокосный");
         }
     }
 
-    public static String checkingOS(int clientOS, int clientDeviceYear) {
+    public static void checkingOS(int clientOS, int clientDeviceYear) {
         if (clientDeviceYear < 2015 && clientOS == 0) {
-            return "Установите облегчённую версию приложения для IOS по ссылке";
+            System.out.println("Установите облегчённую версию приложения для IOS по ссылке");
         } else if (clientOS == 0 && clientDeviceYear > 2015) {
-            return "Установите версию приложения для IOS по ссылке";
+            System.out.println("Установите версию приложения для IOS по ссылке");
         } else if (clientDeviceYear < 2015 && clientOS == 1) {
-            return "Установите облегчённую версию приложения для Android по ссылке";
+            System.out.println("Установите облегчённую версию приложения для Android по ссылке");
         } else {
-            return "Установите версию приложения для Android по ссылке";
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
 
-    public static String calculatingDistance(int distance) {
+    public static int calculatingDistance(int distance) {
         if (distance <= 20) {
-            return "Потребуется дней: 1";
+            return 1;
         } else if (distance > 20 && distance < 60) {
-            return "Потребуется дней: 2";
+            return 2;
         } else if (distance >= 60 && distance < 100) {
-            return "Потребуется дней: 3";
+            return 3;
         } else {
-            return "Доставки нет";
+            return -1;
         }
     }
 
@@ -37,15 +37,22 @@ public class Main {
 
 //        Task 1
 
-        System.out.println(checkingLeapYear(2025));
+        int year = 2025;
+        checkingLeapYear(year);
 
 //        Task 2
 
-        System.out.println(checkingOS(1, 2025));
+        int clientOS = 1;
+        int clientDeviceYear = 2025;
+        checkingOS(clientOS, clientDeviceYear);
 
 //        Task 3
 
-        System.out.println(calculatingDistance(19));
+        int deliveryDistance = 87;
+        int deliveryDays = calculatingDistance(deliveryDistance);
+        if (deliveryDays == -1) {
+            System.out.println("Доставки нет");
+        } else System.out.println("Потребуется дней: " + deliveryDays);
 
     }
 }
